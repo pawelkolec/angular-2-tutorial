a79159c0a945650bb5d363acf84cac0d9f86051c
Paweł Kolec <pawel.kolec@blstream.com>
2017-02-28 15:28:31 +0100

    step 6 part

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -11,8 +11,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
+var router_1 = require('@angular/router');
 var app_component_1 = require('./app.component');
+var heroes_component_1 = require('./heroes.component');
 var hero_detail_component_1 = require('./hero-detail.component');
+var dashboard_component_1 = require('./dashboard.component');
+var hero_service_1 = require('./hero.service');
+var route = router_1.RouterModule.forRoot([
+    {
+        path: 'heroes',
+        component: heroes_component_1.HeroesComponent
+    },
+    {
+        path: 'dashboard',
+        component: dashboard_component_1.DashboardComponent
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+]);
 var AppModule = (function () {
     function AppModule() {
     }
@@ -20,11 +39,17 @@ var AppModule = (function () {
         core_1.NgModule({
             imports: [
                 platform_browser_1.BrowserModule,
-                forms_1.FormsModule
+                forms_1.FormsModule,
+                route
             ],
             declarations: [
                 app_component_1.AppComponent,
-                hero_detail_component_1.HeroDetailComponent
+                heroes_component_1.HeroesComponent,
+                hero_detail_component_1.HeroDetailComponent,
+                dashboard_component_1.DashboardComponent
+            ],
+            providers: [
+                hero_service_1.HeroService
             ],
             bootstrap: [
                 app_component_1.AppComponent
